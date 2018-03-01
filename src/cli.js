1b4219fdd2f299cbde1421e75a1cac15d54c1669
branchard <benoit.branchard@gmail.com>
2018-03-01 11:39:37 +0100

    add more cli options

--- src/cli.js
+++ src/cli.js
@@ -2,29 +2,53 @@
 
 const Api = require("./Api");
 
+
 let args = process.argv.slice(2);
 let token = args[0];
+let restArgs = args.slice(1);
+
+function getArgParam(argName, fullArgName){
+    for (let arg of [argName, fullArgName]) {
+        if(restArgs.includes(arg)){
+            return restArgs[restArgs.indexOf(argName) + 1]
+        }
+    }
+    return undefined;
+}
+
+if(restArgs.includes('-h') || restArgs.includes('--help')){
+    console.log("print help");
+    process.exit(0);
+}
+
 if(!token || typeof token !== "string"){
-    throw "You must define an app token";
+    throw 'You must define an app token';
 }
-let verbose = args[1] == "-v";
 
-try{
-    let api = new Api({
-        token: token,
-        verbose: verbose,
-        timeout: 10000,
-        urlCount: 5,
-        bufferSize: 8,
-        https: true,
-        unit: Api.UNITS.Mbps
-    });
+let verbose = restArgs.includes('-v') || restArgs.includes('--verbose');
+let https = !restArgs.includes('-n') && !restArgs.includes('--no-https');
 
-    api.getSpeed().then(s => {
-        console.log(`Speed: ${s} Mbps`);
-    }).catch(e => {
-        console.error(e.message);
-    });
-}catch(e){
+let timeout = getArgParam('-t', '--timeout');
+let urlCount = getArgParam('-c', '--count');
+let bufferSize = getArgParam('-b', '--buffer');
+let unitName = getArgParam('-u', '--unit');
+if(!(unitName in Api.UNITS)){
+    throw `Unit not valide, must be one of ${Object.keys(Api.UNITS)}`;
+}
+let unit = Api.UNITS[unitName] || Api.UNITS.Mbps;
+
+let api = new Api({
+    token: token,
+    verbose: verbose,
+    timeout: timeout,
+    urlCount: urlCount,
+    bufferSize: bufferSize,
+    https: https,
+    unit: unit
+});
+
+api.getSpeed().then(s => {
+    console.log(`Speed: ${s} ${unit.name}`);
+}).catch(e => {
     console.error(e.message);
-}
+});
